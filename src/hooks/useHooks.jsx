dd91959889a695a9df9c2da2d5e084554682a834
kamalcodezen <kamaluddin7908@gmail.com>
2026-05-07 02:18:26 +0530

    loading state create

--- src/hooks/useHooks.jsx
+++ src/hooks/useHooks.jsx
@@ -2,17 +2,21 @@ import { useEffect, useState } from "react";
 
 const useHooks = () => {
   const [allFriends, setAllFriends] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const friendsDataFetch = async () => {
       const res = await fetch("/friendsData.json");
       const data = await res.json();
-      setAllFriends(data);
+      setTimeout(() => {
+        setAllFriends(data);
+        setLoading(false);
+      }, 1000);
     };
     friendsDataFetch();
   }, []);
 
-  return { allFriends };
+  return { allFriends,loading };
 };
 
 export default useHooks;
