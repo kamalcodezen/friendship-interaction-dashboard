c3fdd782a4fc91d3c746187cb1a10afc07a2540b
kamalcodezen <kamaluddin7908@gmail.com>
2026-05-08 16:03:27 +0530

    scroolToTop added component

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router";
 import Navbar from "../components/shared/Navbar";
 import Footer from "../components/shared/Footer";
+import ScrollToTop from "../scrollToTop/ScrollToTop";
+
 
 const RootLayout = () => {
   return (
     <div className="bg-slate-100">
+      <ScrollToTop />
       <Navbar />
       <Outlet />
       <Footer />
